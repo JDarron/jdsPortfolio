56d69e8ed4d5495eee4fcfe04d38b9925a9025e9
Joshua Spears <j.dspears@yahoo.com>
2019-10-17 20:05:02 -0700

    Fixed the ask me mutton

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // DEPENDENCIES
 require('dotenv').config()
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require('express')
+const bodyParser = require('body-parser')
 const PORT = process.env.PORT || 3001
 const app = express()
 const nodemailer = require('nodemailer')
@@ -24,28 +24,36 @@ const sendEmail = function (req, res) {
 	const name = req.body.name
 	const message = req.body.message
 
-	const mailOptions = {
-		from: email,
-		to: 'j.dspears@yahoo.com',
-		subject: `Contact form submitted by: ${name}` ,
-		text: 	'You have received a new message. \n\n' + 
-				`Here are the details:\n \nName: ${name} \n ` + 
+	if (email === '' || 
+		email === null || 
+		name === '' || 
+		name === null || 
+		message === '' ||
+		message === null) res.status(500).send('Error')
+	else {
+		const mailOptions = {
+			from: email,
+			to: 'j.dspears@yahoo.com',
+			subject: `Contact form submitted by: ${name}`,
+			text: 'You have received a new message. \n\n' +
+				`Here are the details:\n \nName: ${name} \n ` +
 				`Email: ${email}\n Message: \n ${message}`
-	}
-
-	transporter.sendMail(mailOptions, function (error, info) {
-		if (error) {
-			console.log(error)
-			res.status(500).send("Success")
 		}
-		else res.send("Success")
-	})
+
+		transporter.sendMail(mailOptions, function (error, info) {
+			if (error) {
+				console.log(error)
+				res.status(500).send('Success')
+			}
+			else res.send('Success')
+		})
+	}
 }
 
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(express.static("public"))
+app.use(express.static('public'))
 
 
 app.post('/email', sendEmail)
